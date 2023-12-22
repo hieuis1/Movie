3fc60bd3747d4ceffa78a2c8c31f0090a81a56a5
Hieu <hieuop15@gmail.con>
2023-12-23 01:10:59 +0700

    update code

--- src/modules/ticket/Ticket.jsx
+++ src/modules/ticket/Ticket.jsx
@@ -23,99 +23,109 @@ const Ticket = () => {
   } else {
     return (
       <div id="ticket">
-        <Container>
-          <div className="ticket-order">
-            <div className="order-left">
-              <div className="screen">
-                <div className="loaiGhe">
-                  <div className="loaiGhe-container">
-                    <div className="thuong">
-                      <img
-                        src="https://www.betacinemas.vn/Assets/global/img/booking/seat-unselect-normal.png"
-                        alt=""
-                      />
-                      <p>Ghế thường</p>
-                    </div>
-                    <div className="vip">
-                      <img
-                        src="https://www.betacinemas.vn/Assets/global/img/booking/seat-unselect-vip.png"
-                        alt=""
-                      />
-                      <p>Ghế vip</p>
-                    </div>
+        <div className="ticket-order">
+          <div className="order-left">
+            <div className="screen">
+              <div className="loaiGhe">
+                <div className="loaiGhe-container">
+                  <div className="thuong">
+                    <img
+                      src="https://www.betacinemas.vn/Assets/global/img/booking/seat-unselect-normal.png"
+                      alt=""
+                    />
+                    <p>Ghế thường</p>
                   </div>
-                  <div className="canBuy-container">
-                    <div className="canBuy">
-                      <img
-                        src="https://www.betacinemas.vn/Assets/global/img/booking/seat-unselect-normal.png"
-                        alt=""
-                      />
-                      <p>Ghế trống</p>
-                    </div>
-
-                    <div className="isSelect">
-                      <img
-                        src="https://www.betacinemas.vn/Assets/global/img/booking/seat-select-normal.png"
-                        alt=""
-                      />
-                      <p>Ghế đang chọn</p>
-                    </div>
-
-                    <div className="isSold">
-                      <img
-                        src="https://www.betacinemas.vn/Assets/global/img/booking/seat-buy-normal.png"
-                        alt=""
-                      />
-                      <p>Ghế đã bán</p>
-                    </div>
+                  <div className="vip">
+                    <img
+                      src="https://www.betacinemas.vn/Assets/global/img/booking/seat-unselect-vip.png"
+                      alt=""
+                    />
+                    <p>Ghế vip</p>
                   </div>
                 </div>
-                <div className="movie-screen">
-                  <img
-                    src="https://www.betacinemas.vn/Assets/global/img/booking/ic-screen.png"
-                    alt=""
-                    style={{ width: "100%" }}
-                  />
+                <div className="canBuy-container">
+                  <div className="canBuy">
+                    <img
+                      src="https://www.betacinemas.vn/Assets/global/img/booking/seat-unselect-normal.png"
+                      alt=""
+                    />
+                    <p>Ghế trống</p>
+                  </div>
+
+                  <div className="isSelect">
+                    <img
+                      src="https://www.betacinemas.vn/Assets/global/img/booking/seat-select-normal.png"
+                      alt=""
+                    />
+                    <p>Ghế đang chọn</p>
+                  </div>
+
+                  <div className="isSold">
+                    <img
+                      src="https://www.betacinemas.vn/Assets/global/img/booking/seat-buy-normal.png"
+                      alt=""
+                    />
+                    <p>Ghế đã bán</p>
+                  </div>
                 </div>
               </div>
-              <div className="all-chair">
-                {data.danhSachGhe
-                  ? data.danhSachGhe.map((item, index) => {
-                      let a = "";
-                      let b = "chair";
-                      let c = buyArr.findIndex(
-                        (ghe) => ghe.maGhe == item.maGhe
-                      );
-                      if (c != -1 && item.loaiGhe == "Vip") {
-                        a = "buy-vip";
-                      }
-                      if (c != -1 && item.loaiGhe != "Vip") {
-                        a = "buy";
-                      } else if (item.taiKhoanNguoiDat != null) {
-                        a = "sold-out";
-                      }
-                      if (item.loaiGhe == "Vip") {
-                        b = "chair-vip";
-                      }
-                      return (
-                        <button
-                          className={a}
-                          key={index}
-                          id={b}
-                          onClick={() => disPatch(ADD_BUY(item))}
-                        >
-                          {item.tenGhe}
-                        </button>
-                      );
-                    })
-                  : ""}
+              <div className="movie-screen">
+                <img
+                  src="https://www.betacinemas.vn/Assets/global/img/booking/ic-screen.png"
+                  alt=""
+                  style={{ width: "100%" }}
+                />
               </div>
             </div>
-            <div className="order-right">
-              <DetailTicket data={data}></DetailTicket>
+            <div className="all-chair">
+              {data.danhSachGhe
+                ? data.danhSachGhe.map((item, index) => {
+                    let a = "";
+                    let b = "chair";
+                    let c = buyArr.findIndex((ghe) => ghe.maGhe == item.maGhe);
+                    if (c != -1 && item.loaiGhe == "Vip") {
+                      a = "buy-vip";
+                    }
+                    if (c != -1 && item.loaiGhe != "Vip") {
+                      a = "buy";
+                    }
+                    if (
+                      item.taiKhoanNguoiDat != null &&
+                      item.loaiGhe != "Vip"
+                    ) {
+                      a = "sold-out";
+                    }
+                    if (
+                      item.taiKhoanNguoiDat != null &&
+                      item.loaiGhe == "Vip"
+                    ) {
+                      a = "sold-out-vip";
+                    }
+                    if (item.loaiGhe == "Vip") {
+                      b = "chair-vip";
+                    }
+                    return (
+                      <button
+                        className={a}
+                        key={index}
+                        id={b}
+                        onClick={
+                          item.taiKhoanNguoiDat == null
+                            ? () => disPatch(ADD_BUY(item))
+                            : ""
+                        }
+                      >
+                        {item.tenGhe}
+                      </button>
+                    );
+                  })
+                : ""}
             </div>
           </div>
-        </Container>
+          <div className="order-right">
+            <DetailTicket data={data}></DetailTicket>
+          </div>
+        </div>
       </div>
     );
   }
--- src/modules/ticket/detailTicket/DetailTicket.jsx
+++ src/modules/ticket/detailTicket/DetailTicket.jsx
@@ -1,6 +1,9 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const DetailTicket = ({ data }) => {
+  const buyArr = useSelector((state) => state.buy.buy);
   return (
     <div id="detail-ticket">
       <div className="detail-header">
@@ -14,7 +17,7 @@ const DetailTicket = ({ data }) => {
           <p>2D Phụ đề</p>
         </div>
       </div>
-      <div className="detail-body1 container">
+      <div className="detail-body1">
         <div className="theloai">
           <div className="theloai-left">
             <div className="theloai-left-item">
@@ -25,6 +28,103 @@ const DetailTicket = ({ data }) => {
             <p>Hành động</p>
           </div>
         </div>
+      </div>
+      <div className="detail-body2">
+        <div className="thoiLuong">
+          <div className="thoiLuong-left">
+            <div className="thoiLuong-left-item">
+              <i class="fa-solid fa-clock"></i> Thời lượng
+            </div>
+          </div>
+          <div className="thoiLuong-right">
+            <p>124 phút</p>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="detail-body3">
+        <div className="rapChieu">
+          <div className="rapChieu-left">
+            <div className="rapChieu-left-item">
+              <i class="fa fa-institution"></i> Rạp chiếu
+            </div>
+          </div>
+          <div className="rapChieu-right">
+            <p>{data.thongTinPhim.tenCumRap}</p>
+          </div>
+        </div>
+      </div>
+      <div className="detail-body4">
+        <div className="ngayChieu">
+          <div className="ngayChieu-left">
+            <div className="ngayChieu-left-item">
+              <i style={{ marginRight: "3px" }} class="fa fa-calendar"></i>Ngày
+              chiếu
+            </div>
+          </div>
+          <div className="ngayChieu-right">
+            <p>{data.thongTinPhim.ngayChieu}</p>
+          </div>
+        </div>
+      </div>
+      <div className="detail-body5">
+        <div className="gioChieu">
+          <div className="gioChieu-left">
+            <div className="gioChieu-left-item">
+              <i class="fa-solid fa-clock"></i> Giờ chiếu
+            </div>
+          </div>
+          <div className="gioChieu-right">
+            <p>{data.thongTinPhim.gioChieu}</p>
+          </div>
+        </div>
+      </div>
+      <div className="detail-body6">
+        <div className="phongChieu">
+          <div className="phongChieu-left">
+            <div className="phongChieu-left-item">
+              <i class="fa fa-desktop"></i> Phòng chiếu
+            </div>
+          </div>
+          <div className="phongChieu-right">
+            <p>{data.thongTinPhim.tenRap}</p>
+          </div>
+        </div>
+      </div>
+      <div className="detail-body7">
+        <div className="diaChi">
+          <div className="diaChi-left" style={{ width: "50%" }}>
+            <div className="diaChi-left-item">
+              <i class="fa-solid fa-location-dot"></i> Địa chỉ
+            </div>
+          </div>
+          <div className="diaChi-right">
+            <p>{data.thongTinPhim.diaChi}</p>
+          </div>
+        </div>
+      </div>
+      <div className="detail-body8">
+        <div className="gheNgoi">
+          <div className="gheNgoi-left" style={{ width: "50%" }}>
+            <div className="gheNgoi-left-item">
+              <i class="fa-solid fa-location-dot"></i> Ghế Ngồi
+            </div>
+          </div>
+          <div className="gheNgoi-right" style={{ width: "50%" }}>
+            <div className="gheNgoi-right-item">
+              {buyArr.map((item, index) => {
+                return (
+                  <p style={{ marginBottom: "3px" }} key={item.maGhe}>
+                    {item.tenGhe}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="button-buy">
+        <Button variant="contained">Mua ({buyArr.length} vé)</Button>
       </div>
     </div>
   );
